470f311bf7a2dfc9f82c73e918fe84d2c356dec7
tomkoom <tomash.sugint@gmail.com>
2021-09-07 23:59:00 +0300

    feat: make count-cats subtask

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,6 +15,22 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let counter = 0;
+  let matrix = Array.from(arguments).flat(1);
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '^^') {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
 }
+
+// console.log(countCats([
+//   [0, 1, '^^'],
+//   [0, '^^', 2],
+//   ['^^', 1, 2]
+// ]));
